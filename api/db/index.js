dc55a02a60423df21176abc9c3a88155b94f9dac
Guillermo <77939095+guillesotelo@users.noreply.github.com>
2022-01-26 21:58:41 -0300

    Update index.js

--- api/db/index.js
+++ api/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://REMOVED_SECRET";
+const uri = process.env.URI;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('DB Connected to Cluster!'))
 .catch(err => { console.log('Error connecting to Cluster', err) }); 
